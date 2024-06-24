53c703703e1dc4f21114aac1d566941f146d601a
Sam <samcp210@gmail.com>
2024-06-23 19:02:14 -0500

    First Commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "About",
+  description: "This is the about page",
+  keywords: ["about", "page"],
+};
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </div>
+  );
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+      <div>
+        <h1>Pricing</h1>
+        <p>This is the pricing page</p>
+      </div>
+    );
+  }
+  
